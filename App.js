7da6bf7c9cdbb4298adad4db029bb65119381563
furkanucgull <furkanucgull@gmail.com>
2024-06-18 20:28:43 +0200

    Done

---
+++ App.js
@@ -0,0 +1,19 @@
+const display = document.getElementById('input');
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (e) {
+    display.value = 'Error';
+  }
+}
